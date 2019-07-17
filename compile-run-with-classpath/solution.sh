73fe48aab79c0619ff4f9aacbe6f6a8bfd59644a
Bo Zhang <bo@gradle.com>
2019-07-17 22:37:10 +0800

    Only commit solution.sh (#10)
    Only commit solution.sh (#10)

--- compile-run-with-classpath/solution.sh
+++ compile-run-with-classpath/solution.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env sh
+javac -cp commons-lang3-3.9.jar:. StringIsBlank.java
+java -cp commons-lang3-3.9.jar:. StringIsBlank '' ' ' '*'
 # 请在这里编写两条命令：
 # 1. 在当前目录中，使用javac命令编译StringIsBlank.java
 # 2. 在当前目录中，使用java命令运行StringIsBlank，并传递三个参数：第一个参数是一个空字符串，第二个参数是一个空格，第三个参数是一个星号字符*
