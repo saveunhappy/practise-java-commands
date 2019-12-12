bfc2dc75d6bf21485bb7364dc9deaf0bdb346b27
Maricaya <43136257+Maricaya@users.noreply.github.com>
2019-12-12 13:43:03 +0800

    feat: java commands (#63)
    feat: java commands (#63)

--- system-out-system-err/solution.sh
+++ system-out-system-err/solution.sh
@@ -2,3 +2,5 @@
 # 请在这里编写两条命令：
 # 1. 在当前目录中，使用javac命令编译PrintSystemOutSystemErr.java
 # 2. 在当前目录中，使用java命令运行PrintSystemOutSystemErr，使其标准输出和标准错误都重定向到当前目录下一个名为output.txt的文件中
+javac PrintSystemOutSystemErr.java
+java  PrintSystemOutSystemErr > output.txt 2>&1
