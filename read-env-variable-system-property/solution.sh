27d51dfdb03590a8aafe3ed6c86238ef7d6b4afe
linsinan1995 <47880367+linsinan1995@users.noreply.github.com>
2019-12-29 07:46:51 +0800

    java commands (#69)
    java commands (#69)

--- read-env-variable-system-property/solution.sh
+++ read-env-variable-system-property/solution.sh
@@ -6,3 +6,6 @@
 # 使得该java命令输出：
 # Environment variable AAA: $A;
 # System property BBB: '
+javac ReadEnvironmentVariableAndSystemProperty.java
+export AAA="\$A;"
+java -DBBB="'" ReadEnvironmentVariableAndSystemProperty
