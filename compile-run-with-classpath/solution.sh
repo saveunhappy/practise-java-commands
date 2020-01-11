1245432f551c33a03cfe283ce54aa28bf6cc0a61
richard Hu <huyongjinnuaa@163.com>
2020-01-11 14:42:58 +0800

    Richard1230 javac java cmd (#84)
    Richard1230 javac java cmd (#84)

--- compile-run-with-classpath/solution.sh
+++ compile-run-with-classpath/solution.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
-# 请在这里编写两条命令：
+# 请在这里编写两命令：
 # 1. 在当前目录中，使用javac命令编译StringIsBlank.java
 # 2. 在当前目录中，使用java命令运行StringIsBlank，并传递三个参数：第一个参数是一个空字符串，第二个参数是一个空格，第三个参数是一个星号字符*
 # 使得该java命令输出：
@@ -7,3 +7,5 @@
 # First argument is blank: true
 # Second argument is blank: true
 # Third argument is blank: false
+javac -cp commons-lang3-3.9.jar StringIsBlank.java
+java -cp commons-lang3-3.9.jar:. StringIsBlank '' ' ' '*'
