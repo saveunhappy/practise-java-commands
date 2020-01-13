5bcbf6fec78e9546c2b85fa75ade05c182f90dcc
JaneWangCoA <43361638+JaneWangCOA@users.noreply.github.com>
2020-01-13 13:19:13 +0800

    javac与java命令练习 (#87)
    javac与java命令练习 (#87)

--- compile-run-with-classpath/solution.sh
+++ compile-run-with-classpath/solution.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env sh
 # 请在这里编写两条命令：
 # 1. 在当前目录中，使用javac命令编译StringIsBlank.java
+javac -cp commons-lang3-3.9.jar StringIsBlank.java
 # 2. 在当前目录中，使用java命令运行StringIsBlank，并传递三个参数：第一个参数是一个空字符串，第二个参数是一个空格，第三个参数是一个星号字符*
+java -cp commons-lang3-3.9.jar:. StringIsBlank '' ' ' '*'
 # 使得该java命令输出：
 # Args size: 3
 # First argument is blank: true
