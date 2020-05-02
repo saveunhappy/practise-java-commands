731af931da6c0dd9ab8b67d9bd33da229c9ada15
郑海川 <53638659+lance-zheng66@users.noreply.github.com>
2020-05-02 15:01:29 +0800

    命令行练习 (#139)
    命令行练习 (#139)

--- compile-run-with-classpath/solution.sh
+++ compile-run-with-classpath/solution.sh
@@ -7,3 +7,6 @@
 # First argument is blank: true
 # Second argument is blank: true
 # Third argument is blank: false
+javac -cp ./commons-lang3-3.9.jar StringIsBlank.java
+java -cp ./commons-lang3-3.9.jar:. StringIsBlank '' ' ' "'*'"
+
