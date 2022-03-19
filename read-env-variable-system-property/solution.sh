9b1cebc3b4d1e1e657ce86d359639468efbcb89c
yangbisheng <2606424906@qq.com>
2022-03-19 19:15:37 +0800

    命令行 (#216)
    命令行 (#216)

--- read-env-variable-system-property/solution.sh
+++ read-env-variable-system-property/solution.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env sh
 # 请在这里编写三条命令：
 # 1. 导出一个环境变量AAA，值是"$A;"（不包含双引号）
-# 2. 在当前目录中，使用javac命令编译ReadEnvironmentVariableAndSystemProperty.java
-# 3. 在当前目录中，使用java命令运行ReadEnvironmentVariableAndSystemProperty，并传递系统属性（System property）BBB，值为'（一个单引号）
-# 使得该java命令输出：
-# Environment variable AAA: $A;
-# System property BBB: '
+export AAA='$A;'
+javac ./ReadEnvironmentVariableAndSystemProperty.java
+java -DBBB="'" ReadEnvironmentVariableAndSystemProperty
